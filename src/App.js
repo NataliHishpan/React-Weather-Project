25dd43b1c2e0707ad23975e0c5687f5dbb17993e
NataliHishpan <nkalabska@gmail.com>
2022-09-29 15:18:18 -0300

    add link to my linkedin

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <p>
-        This progect was coded by Nataliia Hishpan and is
-        <a href="https://github.com/NataliHishpan/React-Weather-Project.git">
+      <h2>Weather app</h2>
+      <footer>
+        This progect was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/natalia-hishpan-91333622a/"
+          target="_blank"
+        >
+          Nataliia Hishpan
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NataliHishpan/React-Weather-Project.git"
+          target="_blank"
+        >
           open-sourse on GitHub
         </a>
-      </p>
+      </footer>
     </div>
   );
 }
