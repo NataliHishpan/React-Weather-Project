6b0c880745042500082049af16d1c008ed812377
NataliHishpan <nkalabska@gmail.com>
2022-09-29 15:08:15 -0300

    add github link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Weather app
+      <p>
+        This progect was coded by Nataliia Hishpan and is
+        <a href="https://github.com/NataliHishpan/React-Weather-Project.git">
+          open-sourse on GitHub
         </a>
-      </header>
+      </p>
     </div>
   );
 }
-
-export default App;
