c2c72b422ae26d16f3a25249048ad6c076a4ba99
NataliHishpan <nkalabska@gmail.com>
2022-09-30 14:51:51 -0300

    create weather.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h2>Weather app</h2>
-      <footer>
-        This progect was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/natalia-hishpan-91333622a/"
-          target="_blank"
-        >
-          Nataliia Hishpan
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/NataliHishpan/React-Weather-Project.git"
-          target="_blank"
-        >
-          open-sourse on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This progect was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/natalia-hishpan-91333622a/"
+            target="_blank"
+          >
+            Nataliia Hishpan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NataliHishpan/React-Weather-Project.git"
+            target="_blank"
+          >
+            open-sourse on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
